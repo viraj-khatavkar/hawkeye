ba6c9f27281e3bd25ac0490709fda08789810eeb
Viraj Khatavkar <viraj.2438@gmail.com>
2015-03-17 12:17:30 +0530

    Bug fixes

--- src/Hawkeye/views/generators/migration.blade.php
+++ src/Hawkeye/views/generators/migration.blade.php
@@ -5,32 +5,32 @@ use Illuminate\Database\Schema\Blueprint;
 
 class HawkeyeSetupTables extends Migration
 {
-/**
-* Run the migrations.
-*
-* @return void
-*/
-public function up()
-{
-// Create table for storing roles
-Schema::create('{{ $filesTable }}', function (Blueprint $table) {
-$table->bigIncrements('id');
-$table->string('name', 500)->;
-$table->string('extension', 50);
-$table->string('size', 50);
-$table->string('ip', 50);
-$table->dateTime('uploaded_at');
-});
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Create table for storing roles
+        Schema::create('{{ $filesTable }}', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 500);
+            $table->string('extension', 50);
+            $table->string('size', 50);
+            $table->string('ip', 50);
+            $table->dateTime('uploaded_at');
+        });
 
-}
+    }
 
-/**
-* Reverse the migrations.
-*
-* @return void
-*/
-public function down()
-{
-Schema::drop('{{ $filesTable }}');
-}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('{{ $filesTable }}');
+    }
 }
