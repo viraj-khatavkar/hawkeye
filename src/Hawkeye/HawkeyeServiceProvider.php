9f501030a08d33145841c68bef3a03b21d98041c
Viraj Khatavkar <viraj.2438@gmail.com>
2015-03-31 18:08:51 +0530

    Migration issue resolved

--- src/Hawkeye/HawkeyeServiceProvider.php
+++ src/Hawkeye/HawkeyeServiceProvider.php
@@ -30,7 +30,7 @@ class HawkeyeServiceProvider extends ServiceProvider
         });
 
         $this->app->bindShared('command.hawkeye.migration', function ($app) {
-            return new Viraj\Hawkeye\commands\MigrationCommand();
+            return new \Viraj\Hawkeye\commands\MigrationCommand();
         });
     }
 
