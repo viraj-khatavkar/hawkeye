6348d1fabca669e0b850694c0968f9119aec1981
Viraj Khatavkar <viraj.2438@gmail.com>
2015-11-20 01:43:35 +0530

    Helper function to get full image path

--- src/Hawkeye/HawkeyeTrait.php
+++ src/Hawkeye/HawkeyeTrait.php
@@ -5,12 +5,27 @@ use Viraj\Hawkeye\Exceptions\InvalidMd5HashException;
 
 trait HawkeyeTrait
 {
+    public function generateFullImagePath($image_name)
+    {
+        $file = explode('.', $image_name);
+        $file_meta = explode('_', $file[0]);
+
+        $directoryPath = $this->generateDirectoryPathFromName($file_meta[0]);
+
+        return $directoryPath . '/' . $image_name;
+    }
+
     public function generateDirectoryPathFromName($hashedFileName)
     {
         if ($this->isValidMd5Name($hashedFileName)) {
             return config('hawkeye.hawkeye_base_path') . implode('/', str_split(substr($hashedFileName, 0, 30), 3));
         }
         throw new InvalidMd5HashException("The hashed File Name is not a valid  md5 hash.");
+    }
+
+    public function isValidMd5Name($fileName)
+    {
+        return strlen($fileName) == 32 && ctype_xdigit($fileName);
     }
 
     public function createDirectory($directoryPath)
@@ -20,10 +35,5 @@ trait HawkeyeTrait
                 throw new DiretoryNotCreatedException("Unable to create directory.");
             }//end if
         }//end if
-    }
-
-    public function isValidMd5Name($fileName)
-    {
-        return strlen($fileName) == 32 && ctype_xdigit($fileName);
     }
 }
