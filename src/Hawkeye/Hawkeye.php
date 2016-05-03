ab61da9ce1abd50c5fe72fec11547e7018d98715
Viraj Khatavkar <viraj.2438@gmail.com>
2016-05-03 09:18:35 +0530

    Added a fit method to maintain aspect ratio

--- src/Hawkeye/Hawkeye.php
+++ src/Hawkeye/Hawkeye.php
@@ -135,6 +135,54 @@ class Hawkeye
 
         return $this;
     }
+    
+    public function fit($options = [])
+    {
+        /**
+         * Looping through all the image types of configuration and setting the image types as specified in options
+         * array. If options array is empty, then all the image types from configuration will be considered. These
+         * image types array will be then used to decide the resize dimensions for an uploaded image.
+         */
+        if (count($options) > 0) {
+            foreach (config('hawkeye.images') as $key => $value) {
+                if (in_array($key, $options)) {
+                    $images_types[$key] = $value;
+                }
+            }
+        } else {
+            $images_types = config('hawkeye.images');
+        }
+
+        $count = 0;
+
+        //Looping through all the uploaded files to resize them
+        foreach ($this->uploadedFiles['separated'] as $file) {
+
+            //Retrieving file extension, hashed_name, directory path to create a new name for resized image.
+            $file_meta = explode('.', $file['original']);
+            $hash = $file_meta[0];
+            $directoryPath = $this->generateDirectoryPathFromName($hash);
+            $file_name = $directoryPath . '/' . $file['original'];
+
+            //Looping through all the image types for which image needs to be resized.
+            foreach ($images_types as $key => $value) {
+                $dimensions = explode('x', $value);
+
+                $scaled_image_name = $hash . '_' . $dimensions[0] . '_' . $dimensions[1] . '.' . $file_meta[1];
+                $scaled_image_path = $directoryPath . '/' . $scaled_image_name;
+
+                Image::make($file_name)
+                     ->fit($dimensions[0], $dimensions[1])
+                     ->save($scaled_image_path);
+
+                $this->uploadedFiles['separated'][$count][$key] = $scaled_image_name;
+                array_push($this->uploadedFiles['list'], $scaled_image_name);
+            }
+            $count++;
+        }
+
+        return $this;
+    }
 
     public function get()
     {
